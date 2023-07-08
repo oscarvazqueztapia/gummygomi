ed44ddeb6b23438282d89d19f803f06197afe804
oscar vazquez <solopordiversion2@outlook.com>
2023-07-08 15:11:46 -0700

    se agrego javascript y div de totales a pagina checkout

---
+++ gummy.js
@@ -0,0 +1,9 @@
+const p = document.getElementById("parrafo")
+const p2 = document.getElementById("parrafo2")
+
+
+p.innerText = "Esto es un texto simple"
+p2.innerHTML = "<h2>Esto es inserción de <code>html</code></h2>"
+<p id="parrafo"></p>
+
+<p id="parrafo2"></p>
