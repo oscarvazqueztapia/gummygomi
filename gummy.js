a1a46a5da8c708367a720a1788de91da975df901
oscar vazquez <solopordiversion2@outlook.com>
2023-07-10 03:20:39 -0700

    se agregaron modificaciones a css para modo responsivo en seccion quienSomos y header

--- gummy.js
+++ gummy.js
@@ -7,3 +7,21 @@ p2.innerHTML = "<h2>Esto es inserción de <code>html</code></h2>"
 <p id="parrafo"></p>
 
 <p id="parrafo2"></p>
+
+const value1 = document.getElementById('value1');
+const value2 = document.getElementById('value2');
+const sumar = document.getElementById('sumar');
+let resultado = document.getElementById('resultado');
+
+sumar.addEventListener('click', () => {
+  let v1 = parseInt(value1.value)
+  let v2 = parseInt(value2.value)
+  
+  console.log(v1+v2)
+  
+  resultado.value = v1 + v2;
+});
+<input id="value1">
+<input id="value2">
+<button id="sumar">Sumar</button>
+<input id="resultado" value=""></input>
